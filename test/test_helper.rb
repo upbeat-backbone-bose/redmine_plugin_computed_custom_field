4743b621f26fb38cb058fe09e26fe27f6593fd27
upbeat-backbone-bose <438092+upbeat-backbone-bose@users.noreply.github.com>
2026-03-16 15:47:24 +0800

    Ensure plugin patches load in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 require File.expand_path('../fixtures_helper', __FILE__)
 require File.expand_path('../methods_helper', __FILE__)
 require File.expand_path('../../init', __FILE__)
+require 'computed_custom_field/custom_field_patch'
+require 'computed_custom_field/custom_fields_helper_patch'
+require 'computed_custom_field/issue_patch'
+require 'computed_custom_field/model_patch'
+
+ComputedCustomField.patch_models
 
 class ComputedCustomFieldTestCase < ActiveSupport::TestCase
   fixtures FixturesHelper.fixtures
