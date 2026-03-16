77f6897f57ef95a44d84f6d82a8c2031f6cee860
upbeat-backbone-bose <438092+upbeat-backbone-bose@users.noreply.github.com>
2026-03-16 15:44:36 +0800

    Load plugin init in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 require File.expand_path('../fixtures_helper', __FILE__)
 require File.expand_path('../methods_helper', __FILE__)
+require File.expand_path('../../init', __FILE__)
 
 class ComputedCustomFieldTestCase < ActiveSupport::TestCase
   fixtures FixturesHelper.fixtures
